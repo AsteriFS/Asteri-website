18f226b6a09fa3612cc91ef59265474571b52ce1
Milos-GoodGuy <milos@goodguys.nu>
2024-01-29 14:39:21 +0100

    carrier form  + booking popup

--- src/js/karriar.js
+++ src/js/karriar.js
@@ -2,7 +2,9 @@ import '../karriar.scss'
 
 import './components/excerpt.js'
 
-document.addEventListener('DOMContentLoaded', function () {
+
+
+
   let category = document.querySelectorAll('.collection-list input[type="checkbox"]');
   let allCategory = document.getElementById('all-categories');
 
@@ -18,7 +20,131 @@ document.addEventListener('DOMContentLoaded', function () {
   category.forEach(function (checkbox) {
     checkbox.addEventListener('change', handleCheckboxChange);
   });
-});
+
+
+//////////////////
+
+  // swiper js
+  var swiper = new Swiper(".myswiper", {
+    allowTouchMove: false,
+  });
+
+  let nextButton = document.querySelector('.swiper-next');
+  let prevButton = document.querySelector('.swiper-prev');
+  let slideNumber = 1;
+
+  function checkFields(direction){
+     // Get the currently active slide
+     var activeSlide = document.querySelector('.swiper-slide-active');
+
+     // select all rows with input fields
+     var formRow = activeSlide.querySelectorAll('.contact-form-input-row');
+     let hasError = false;
+ 
+     // loop through each row and check its input fields and checkboxes
+     // to see if they are not empty
+     for (let j = 0; j < formRow.length; j++) {
+       let selectFields = formRow[j].querySelectorAll('select');
+       let inputFields = formRow[j].querySelectorAll('input[type="text"]');
+       let errorText = formRow[j].querySelector('.contact-form_error-text');
+       var checkboxes = formRow[j].querySelectorAll('input[type="checkbox"]');
+       let checked = false;
+ 
+       // Loop through each select field if there is any
+       if(selectFields.length > 0){
+         selectFields.forEach(function (selectField) {
+           // Check if the input field has value
+           if (selectField.value) {
+             errorText.style.display = 'none';
+             hasError = false;
+           } else {
+             errorText.style.display = 'block';
+             hasError = true;
+             return
+           }
+         });
+       }
+ 
+       // Loop through each input field if there is any
+       if(inputFields.length > 0){
+         inputFields.forEach(function (inputField) {
+           // Check if the input field has value
+           if (inputField.value) {
+             errorText.style.display = 'none';
+             hasError = false;
+           } else {
+             errorText.style.display = 'block';
+             hasError = true;
+             return
+           }
+         });
+       }
+ 
+ 
+       // Loop through each checkbox field if there is any
+       if(checkboxes.length > 0){
+         checkboxes.forEach(function (checkbox) {
+           // Check if the checkbox is checked
+           if (checkbox.checked) {
+             checked = true;
+             errorText.style.display = 'none';
+           } 
+         });
+       }
+ 
+ 
+       if(checkboxes.length > 0){
+         if (!checked) {
+           errorText.style.display = 'block';
+           hasError = true
+         } 
+       }
+     }
+ 
+     if(!hasError){
+      if(direction === 'next'){
+        slideNumber++
+        swiper.slideNext();
+        var swiperWrapper = document.querySelector('.swiper-wrapper');
+        // Scroll to the top of the 'swiper-wrapper' element
+        swiperWrapper.scrollIntoView({ behavior: 'smooth' });
+        if(slideNumber === 2){
+          document.querySelector('.contact-form-navigation-block .swiper-prev').style.display = 'flex';
+          document.querySelector('.contact-form-navigation-block .swiper-prev').addEventListener('click', function(){
+            prevSlide();
+          });
+        } else {
+          document.querySelector('.contact-form-navigation-block').style.display = 'none';
+        }
+      } 
+     }
+  }
+
+  function prevSlide(){
+    slideNumber--
+    swiper.slidePrev();
+    var swiperWrapper = document.querySelector('.swiper-wrapper');
+    // Scroll to the top of the 'swiper-wrapper' element
+    swiperWrapper.scrollIntoView({ behavior: 'smooth' });
+    if(slideNumber === 1){
+      document.querySelector('.contact-form-navigation-block .swiper-prev').style.display = 'none';
+    } else {
+      document.querySelector('.contact-form-navigation-block').style.display = 'block';
+    }
+  }
+
+
+  nextButton.addEventListener('click', function() {
+    checkFields('next')
+  });
+
+  prevButton.addEventListener('click', function() {
+    prevSlide()
+  });
+
+
+
+
 
 
 
