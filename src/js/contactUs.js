3459687a9708ee917d817cd0fdbafef0dbc664aa
Milos-GoodGuy <milos@goodguys.nu>
2024-03-12 09:19:15 +0100

    added enhanced conversion to contact form

--- src/js/contactUs.js
+++ src/js/contactUs.js
@@ -1,1 +1,18 @@
 import './components/formValidation.js'
+
+const form = document.getElementById('wf-form-Kontakta-oss');
+form.addEventListener('submit', function(event) {
+  // Get the email and phone number values from the form inputs
+  const email = document.getElementById('Email-3').value;
+  const phoneNumber = document.getElementById('form-phone').value;
+
+
+  // Push data to the dataLayer
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'contact_form_completion',
+    'email': email,
+    'phone_number': phoneNumber
+  });
+
+});
