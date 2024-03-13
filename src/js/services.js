e62438cd583545702b535b6400e91e7ac97e3cd6
Milos-GoodGuy <milos@goodguys.nu>
2024-03-13 08:15:01 +0100

    tjanster single pages added advanced conversion gtm

--- src/js/services.js
+++ src/js/services.js
@@ -10,4 +10,24 @@ $('.w-dyn-bind-empty').parents('.service-faq-list_item').each(function(){
 })
 
 
+///////////////////////////////////////////////
+// code for enhanced conversion tracking
+///////////////////////////////////////////////
+const form = document.getElementById('wf-form-Tj-nster');
+form.addEventListener('submit', function(event) {
+  // Get the email and phone number values from the form inputs
+  const email = document.getElementById('E-post').value;
+  const phoneNumber = document.getElementById('form-phone').value;
+
+
+  // Push data to the dataLayer
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'offer_form_completion',
+    'email': email,
+    'phone_number': phoneNumber
+  });
+
+});
+
 
