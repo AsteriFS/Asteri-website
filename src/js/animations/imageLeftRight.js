9ee6694976d3bbd04207801eceb70924c751ef53
Milos Rancic <milos.rancic@madetight.com>
2024-03-14 10:21:11 +0100

    animation edit

--- src/js/animations/imageLeftRight.js
+++ src/js/animations/imageLeftRight.js
@@ -16,33 +16,29 @@ revealContainers.forEach((container) => {
   
   // Check the data-attribute value and apply animations accordingly
   if (dataAttribute === "left-to-right") { // Move from left to right
-    tl.from(container, {
-      duration: 1,
-      xPercent: -100,
-      ease: Power2.out
-    });
+    tl.fromTo(container, 
+      { xPercent: -100 }, // From
+      { xPercent: 0, duration: 1, ease: Power2.out } // To
+    );
     images.forEach((image) => {
-      tl.from(image, {
-        duration: 1,
-        xPercent: 100,
-        scale: 1.3,
-        ease: Power2.out
-      }, "-=1"); // Animation starts 1 second after the container animation starts
+      tl.fromTo(image, 
+        { xPercent: 100, scale: 1.3 }, // From
+        { xPercent: 0, scale: 1, duration: 1, ease: Power2.out }, // To
+        "-=1" // Animation starts 1 second after the container animation starts
+      );
     });
-  } 
-  else if (dataAttribute === "right-to-left") {
-  	tl.from(container, {
-      duration: 1,
-      xPercent: 100,
-      ease: Power2.out
-    });
+  }
+  else if (dataAttribute === "right-to-left") { // Move from left to right
+    tl.fromTo(container, 
+      { xPercent: 100 }, // From
+      { xPercent: 0, duration: 1, ease: Power2.out } // To
+    );
     images.forEach((image) => {
-      tl.from(image, {
-        duration: 1,
-        xPercent: -100,
-        scale: 1.3,
-        ease: Power2.out
-      }, "-=1"); // Animation starts 1 second after the container animation starts
+      tl.fromTo(image, 
+        { xPercent: -100, scale: 1.3 }, // From
+        { xPercent: 0, scale: 1, duration: 1, ease: Power2.out }, // To
+        "-=1" // Animation starts 1 second after the container animation starts
+      );
     });
-  };
+  }
 });
